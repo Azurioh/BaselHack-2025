2a37d570e64dbb9ab4541d769768b517935ae487
ewan_s <ewan.schaffhauser@gmail.com>
2025-11-01 09:30:26 +0100

    feat: making Questions page use Tailwind CSS and tailwind work

---
+++ code/apps/frontend/src/pages/Questions/Questions.tsx
@@ -0,0 +1,10 @@
+import '../../index.css';
+
+export default function Questions() {
+  return (
+    <div>
+      <h1 className="text-blue-600 dark:text-sky-400">Questions Page</h1>
+      <p>Welcome to BaselHack 2025</p>
+    </div>
+  )
+}
