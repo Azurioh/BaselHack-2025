cee05f40289590ce5c3c8dfd3f79b2ad839f3c2a
Gabriel Leveque <gabriel.leveque@epitech.eu>
2025-11-02 11:30:06 +0100

    feat (frontend): create random account

--- code/apps/frontend/src/pages/CreateAccount.tsx
+++ code/apps/frontend/src/pages/CreateAccount.tsx
@@ -1,7 +1,25 @@
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
+import { useEffect } from 'react';
 
 export function CreateAccount() {
-  const _navigate = useNavigate();
+  const navigate = useNavigate();
   const { randomRegister } = useAuth();
+
+  useEffect(() => {
+    async function createRandomAccount() {
+      try {
+        await randomRegister();
+        navigate('/');
+      } catch (error) {
+        console.error('Account creation failed:', error);
+        navigate('/register');
+      }
+    }
+    createRandomAccount();
+  }, []);
+
+  return (
+    null
+  );
 }
